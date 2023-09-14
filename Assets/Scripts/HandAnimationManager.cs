f459f6f3c6bfe6f8b17a325148ab51f159e4108c
NguyenHieu <hieu72351@gmail.com>
2023-09-14 23:22:02 +0700

    ud chạy chuỗi animation

--- Assets/Scripts/HandAnimationManager.cs
+++ Assets/Scripts/HandAnimationManager.cs
@@ -6,7 +6,13 @@ using UnityEngine;
 public class HandAnimationManager : MonoBehaviour
 {
     public Animator handAnimator;
-    public AnimationClip testAnimation;
+    public AnimationClip[] testAnimations;
+    public float blendTime = 0.3f;
+    public float hideDelay = 3f;
+    public float inbetweenOffset = 0.5f;
+    public float normalizedTransitionDuration;
+    public float normalizedTimeOffset;
+    public float normalizedTransitionTime;
 
     [Button]
     public void PlaySequenceAnimation()
@@ -14,17 +20,23 @@ public class HandAnimationManager : MonoBehaviour
         IEnumerator PlayAnimationRoutine()
         {
             handAnimator.gameObject.SetActive(true);
-            handAnimator.Play(testAnimation.name, 1);
-            handAnimator.SetFloat("Speed", 0);
             handAnimator.Play("HandShow", 0);
-            yield return new WaitForSeconds(0.5f);
-            handAnimator.SetFloat("Speed", 1);
-            yield return new WaitForSeconds(testAnimation.length - 0.1f);
-            handAnimator.SetFloat("Speed", 0);
+            for (int i = 0; i < testAnimations.Length; i++)
+            {
+                if(i == 0) handAnimator.Play(testAnimations[i].name, 1);
+                yield return new WaitForSeconds(testAnimations[i].length - inbetweenOffset);
+                if(i < testAnimations.Length - 1) handAnimator.CrossFade(testAnimations[i + 1].name, normalizedTransitionDuration, 1, normalizedTimeOffset, normalizedTransitionTime);
+            }
+            //handAnimator.SetFloat("Speed", 0);
+            //yield return new WaitForSeconds(blendTime);
+            //handAnimator.SetFloat("Speed", 1);
+            yield return new WaitForSeconds(hideDelay);
+            //handAnimator.SetFloat("Speed", 0);
             handAnimator.Play("HandHide", 0);
+            //yield return new WaitForSeconds(blendTime);
             yield return new WaitForSeconds(0.5f);
-            handAnimator.SetFloat("Speed", 1);
-            handAnimator.gameObject.SetActive(false);
+            //handAnimator.SetFloat("Speed", 1);
+            //handAnimator.gameObject.SetActive(false);
         }
         StartCoroutine(PlayAnimationRoutine()); 
     }
